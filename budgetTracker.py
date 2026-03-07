ee1a4408e01adeb6d181343dbe05d1d4ce13d742
tMiller05 <tm654@kent.ac.uk>
2026-03-07 20:36:47 +0000

    new User

---
+++ budgetTracker.py
@@ -0,0 +1,37 @@
+import re
+import csv
+
+class User:
+    def __init__(self,Name,Email,Pass,Balance):
+        self.Name = Name
+        self.Email = Email
+        self.Pass = Pass
+        self.Balance = float(Balance)
+
+def newUser():
+    print("-- Create Account --")
+    Name=input("Enter Name:   ").title()
+
+    Email=input("Enter Email:   ")
+    if not re.match(r".+@.+",Email):
+        print("Invalid Email, Try Again.")
+        return
+    
+    print("Password must be at least 8 characters with 1 capital Letter and 2 numbers.")
+    Pass=input("Enter Password   ")
+    if not re.match(r"^(?=.*[A-Z])(?=(?:.[0-9]){2})",Pass):
+        print("Passoword not safe, try another.")
+        return
+
+    Balance = input("Enter initial balance:   ")
+    if not re.match(r"^\d+(\.\d{1,2})?$",Balance):
+        print("Incorrect format, use 0.00.")
+        return
+    
+    newUser=User(Name,Email,Pass,Balance)
+    with open ("Users.csv", mode="a", newline="") as file:
+        Writer=csv.writer(file)
+        Writer.writerow([newUser.Name, newUser.Email, newUser.Pass, newUser.Balance])
+    print(f"User {Name} has been added successfully.")
+
+newUser()
